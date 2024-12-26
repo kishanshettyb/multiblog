cb6ee409d6981b32c87a8c29b5086dae74fe9135
kishanshettyb <kishanqr@gmail.com>
2024-12-26 23:46:59 +0530

    feat: add footer component

---
+++ src/components/footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className="w-[100%] py-2 m-auto border flex-1  flex justify-center items-center border-x-0 border-b-0 fixed bottom-0">
+      <p className="text-sm text-slate-500">&copy; copyright 2025. All rights reserved.</p>
+    </div>
+  )
+}
+
+export default Footer
