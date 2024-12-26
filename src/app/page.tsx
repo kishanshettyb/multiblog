4220178339b63df7ff5b7fc18f1fe69828154eb6
kishanshettyb <kishanqr@gmail.com>
2024-12-27 01:35:43 +0530

    fix: fix responsive issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,17 +5,17 @@ import React from 'react'
 
 function Page() {
   return (
-    <div className="flex h-screen w-screen justify-center items-center  ">
-      <div className="w-1/2">
+    <div className="flex flex-col md:flex-row h-screen w-screen justify-center items-center">
+      <div className="w-full h-[40%] md:h-full md:w-1/2">
         <Image
           src="/images/login.jpg"
           width="1080"
           height="1080"
           alt="multiblog"
-          className="w-full h-screen object-cover"
+          className="w-full h-full  md:h-screen object-cover object-top"
         />
       </div>
-      <div className="w-1/2 flex justify-center items-center">
+      <div className="w-full  h-[60%]  md:w-1/2  flex justify-center items-center">
         <LoginForm />
       </div>
     </div>
