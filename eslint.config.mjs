74c58da5b80f83bf116e5ffe4ee79f14bc7e0a17
kishanshettyb <kishanqr@gmail.com>
2024-12-26 20:55:52 +0530

    feat: add husky setup and eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,9 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import reactPlugin from "eslint-plugin-react";
+import reactHooksPlugin from "eslint-plugin-react-hooks";
+import prettierPlugin from "eslint-plugin-prettier";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,8 +12,36 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
+// ESLint Configuration
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends("next/core-web-vitals", "next/typescript", "prettier"),
+  {
+    // Adding plugins
+    plugins: {
+      react: reactPlugin,
+      "react-hooks": reactHooksPlugin,
+      prettier: prettierPlugin,
+    },
+    // Custom rules
+    rules: {
+      quotes: ["error", "single"],
+      semi: ["error", "always"],
+      "no-unused-vars": [
+        "error",
+        {
+          args: "after-used",
+          caughtErrors: "none",
+          ignoreRestSiblings: true,
+          vars: "all",
+        },
+      ],
+      "prefer-const": "error",
+      "react-hooks/exhaustive-deps": "error",
+    },
+  },
 ];
 
-export default eslintConfig;
+// Commitlint Configuration using compat
+
+// Combine ESLint and Commitlint configs
+export default [...eslintConfig];
