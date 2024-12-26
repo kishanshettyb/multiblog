f2c13a92ad7a4d29d4abc7624e8d2d252284589d
kishanshettyb <kishanqr@gmail.com>
2024-12-26 23:50:39 +0530

    feat: add dashboard page

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import DashboardHeader from '@/components/dashboardHeader'
+import React from 'react'
+
+function Page() {
+  return (
+    <div>
+      <DashboardHeader />
+    </div>
+  )
+}
+
+export default Page
