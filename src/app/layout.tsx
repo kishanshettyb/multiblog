637875a9f59d09c393f9e7a452cceb00d391556d
kishanshettyb <kishanqr@gmail.com>
2024-12-27 09:39:15 +0530

    fix: fix layout and wrap with query provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
+import ReactQueryProvider from '@/lib/provider/ReactQueryProvider'
 import './globals.css'
 
 const geistSans = Geist({
@@ -25,7 +26,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <>{children}</>
+        <>
+          <ReactQueryProvider>{children}</ReactQueryProvider>
+        </>
       </body>
     </html>
   )
