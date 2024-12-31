789aecbddf4d6cd82dc267705947a9e4b397ecda
kishanshettyb <kishanqr@gmail.com>
2024-12-31 07:54:00 +0530

    fix: fix login api and mutation

---
+++ src/services/mutations/login.ts
@@ -0,0 +1,36 @@
+import { useMutation } from '@tanstack/react-query'
+import axios from 'axios'
+import { useRouter } from 'next/navigation'
+
+export const useLoginMutation = (setError: unknown) => {
+  const router = useRouter()
+  const LOGIN_URL = process.env.NEXT_PUBLIC_LOGIN_URL
+  if (!LOGIN_URL) {
+    throw new Error('LOGIN_URL is not defined in the environment variables.')
+  }
+
+  return useMutation({
+    mutationFn: async (loginData) => {
+      const response = await axios.post(LOGIN_URL, loginData, {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      })
+      return response.data
+    },
+    onSuccess: async (data) => {
+      console.log(JSON.stringify(data))
+      // todo set cookies
+      router.push('/dashboard')
+    },
+    onError: (error) => {
+      if (axios.isAxiosError(error)) {
+        // Access the error response and set the error message accordingly
+        setError(error.response?.data?.error.message || error.message)
+      } else {
+        console.log('Error:', error) // Handle non-Axios errors
+        setError(error.message)
+      }
+    }
+  })
+}
