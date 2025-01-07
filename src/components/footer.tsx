fc5b3eab301fb67e47f7167a151e55fa0488426c
kishanshettyb <kishanqr@gmail.com>
2025-01-07 07:50:01 +0530

    fix: fix minor issues

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className="w-[100%] py-2 m-auto border flex-1  flex justify-center items-center border-x-0 border-b-0 fixed bottom-0">
+    <div className="w-[100%] py-2 m-auto border flex-1  flex justify-center items-center border-x-0 border-b-0 bg-white fixed bottom-0">
       <p className="text-sm text-slate-500">&copy; copyright 2025. All rights reserved.</p>
     </div>
   )
