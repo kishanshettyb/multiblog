906fa75143deaab39a3ba31724119917f1cc602d
kishanshettyb <kishanqr@gmail.com>
2025-01-07 06:51:32 +0530

    refactor: refactor code

--- src/app/(app)/categories/page.tsx
+++ src/app/(app)/categories/page.tsx
@@ -1,15 +1,14 @@
 'use client'
 import React from 'react'
 import Header from '@/components/header'
-import { useGetAllCategories } from '@/services/queries/categories'
 import { Layers2 } from 'lucide-react'
 import { CustomDataTable } from '@/components/customDatatable'
 import CreateCategoriesForm from '@/components/forms/createCategoriesForm'
 import { getCategoryColumns } from '@/config/categoriesColumns'
+import { useCategories } from '@/hooks/useCategories'
 
 function CategoriesPage() {
-  const allCategoriesData = useGetAllCategories()
-  const data = allCategoriesData?.data?.data || []
+  const { data } = useCategories()
   const columns = getCategoryColumns()
 
   return (
--- src/app/(app)/domains/page.tsx
+++ src/app/(app)/domains/page.tsx
@@ -1,126 +1,15 @@
 'use client'
 import React from 'react'
-import { useGetAllDomains } from '@/services/queries/domains'
-import { Checkbox } from '@/components/ui/checkbox'
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger
-} from '@/components/ui/dropdown-menu'
-import { ArrowUpDown, MoreHorizontal, Pen, Plus } from 'lucide-react'
-import { ColumnDef } from '@tanstack/react-table'
+import { Plus } from 'lucide-react'
 import Header from '@/components/header'
 import { CustomDataTable } from '@/components/customDatatable'
-import { Button } from '@/components/ui/button'
-import { Domain } from '@/types/commonTypes'
 import CreateDomainsForm from '@/components/forms/createDomainsForm'
-import ShowDate from '@/components/showDate'
+import { useDomains } from '@/hooks/useDomains'
+import { getDomainsColumns } from '@/config/domainsColumns'
 
 function Domains() {
-  const allDomainsData = useGetAllDomains()
-  const data = allDomainsData?.data?.data || []
-  const columns: ColumnDef<Domain>[] = [
-    {
-      id: 'select',
-      header: ({ table }) => (
-        <Checkbox
-          checked={
-            table.getIsAllPageRowsSelected() ||
-            (table.getIsSomePageRowsSelected() && 'indeterminate')
-          }
-          onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
-          aria-label="Select all"
-        />
-      ),
-      cell: ({ row }) => (
-        <Checkbox
-          checked={row.getIsSelected()}
-          onCheckedChange={(value) => row.toggleSelected(!!value)}
-          aria-label="Select row"
-        />
-      ),
-      enableSorting: false,
-      enableHiding: false
-    },
-    {
-      accessorKey: 'domain_name',
-      header: 'Domain Name',
-      cell: ({ row }) => <div className="lowercase">{row.getValue('domain_name')}</div>
-    },
-    {
-      accessorKey: 'domain_desc',
-      header: 'Description',
-      cell: ({ row }) => <div className="lowercase">{row.getValue('domain_desc')}</div>
-    },
-    {
-      accessorKey: 'createdAt',
-      header: ({ column }) => (
-        <Button
-          variant="ghost"
-          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
-        >
-          Created Date
-          <ArrowUpDown />
-        </Button>
-      ),
-      cell: ({ row }) => (
-        <ShowDate
-          color="green"
-          createdDate={row.getValue('createdAt')}
-          updatedDate={row.getValue('createdAt')}
-        />
-      )
-    },
-    {
-      accessorKey: 'publishedAt',
-      header: ({ column }) => (
-        <Button
-          variant="ghost"
-          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
-        >
-          Published Date
-          <ArrowUpDown />
-        </Button>
-      ),
-      cell: ({ row }) => (
-        <ShowDate
-          color="orange"
-          createdDate={row.getValue('publishedAt')}
-          updatedDate={row.getValue('publishedAt')}
-        />
-      )
-    },
-    {
-      id: 'actions',
-      enableHiding: false,
-      cell: ({ row }) => {
-        const payment = row.original
-        return (
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button variant="outline" className="h-8 w-8 p-0">
-                <span className="sr-only">Open menu</span>
-                <MoreHorizontal />
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end">
-              <DropdownMenuLabel>Actions</DropdownMenuLabel>
-              <DropdownMenuItem onClick={() => navigator.clipboard.writeText(payment.id)}>
-                <>
-                  <Pen size={10} /> Edit
-                </>
-              </DropdownMenuItem>
-              <DropdownMenuSeparator />
-            </DropdownMenuContent>
-          </DropdownMenu>
-        )
-      }
-    }
-  ]
-
+  const { data } = useDomains()
+  const columns = getDomainsColumns()
   return (
     <div className="h-full overflow-auto ">
       <Header
