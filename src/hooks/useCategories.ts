4e860a4a0a853a0bf1ad9577207cec31004bfec5
kishanshettyb <kishanqr@gmail.com>
2025-01-07 07:40:25 +0530

    fix: fix sorting

--- src/hooks/useCategories.ts
+++ src/hooks/useCategories.ts
@@ -1,8 +1,16 @@
 // src/hooks/useCategories.ts
 import { useGetAllCategories } from '@/services/queries/categories'
+import { Categories } from '@/types/commonTypes'
 
 export const useCategories = () => {
   const allCategoriesData = useGetAllCategories()
-  const data = allCategoriesData?.data?.data || []
-  return { data }
+  const data: Categories[] = allCategoriesData?.data?.data || []
+
+  const sortedData = data.sort((a, b) => {
+    const dateA = new Date(a.createdAt)
+    const dateB = new Date(b.createdAt)
+
+    return dateB.getTime() - dateA.getTime()
+  })
+  return { data: sortedData }
 }
--- src/hooks/useDomains.ts
+++ src/hooks/useDomains.ts
@@ -1,6 +1,15 @@
 import { useGetAllDomains } from '@/services/queries/domains'
+import { Domain } from '@/types/commonTypes'
 export const useDomains = () => {
   const allDomainsData = useGetAllDomains()
-  const data = allDomainsData?.data?.data || []
-  return { data }
+  const data: Domain[] = allDomainsData?.data?.data || []
+
+  const sortedData = data.sort((a, b) => {
+    const dateA = new Date(a.createdAt)
+    const dateB = new Date(b.createdAt)
+
+    return dateB.getTime() - dateA.getTime()
+  })
+
+  return { data: sortedData }
 }
