60c3307c85ecf37639ea47ee2000d92f26da7623
kishanshettyb <kishanqr@gmail.com>
2025-01-09 06:55:31 +0530

    fix: fix tag hook

--- src/hooks/useTags.ts
+++ src/hooks/useTags.ts
@@ -1,16 +1,16 @@
-import { useGetAllTags } from '@/services/queries/tags'
-import { Tags } from '@/types/commonTypes'
+import { useGetAllTags } from "@/services/queries/tags";
+import { Tags } from "@/types/commonTypes";
 
 export const useTags = () => {
-  const allTagsData = useGetAllTags()
-  const data: Tags[] = allTagsData?.data?.data || []
+  const allTagsData = useGetAllTags();
+  const data: Tags[] = allTagsData?.data?.data || [];
 
   const sortedData = data.sort((a, b) => {
-    const dateA = new Date(a.createdAt)
-    const dateB = new Date(b.createdAt)
+    const dateA = new Date(a.createdAt);
+    const dateB = new Date(b.createdAt);
 
-    return dateB.getTime() - dateA.getTime()
-  })
+    return dateB.getTime() - dateA.getTime();
+  });
 
-  return { data: sortedData }
-}
+  return { data: sortedData };
+};
