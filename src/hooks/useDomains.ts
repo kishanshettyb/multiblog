49087b9f3baceb396baec9838e6bbbe6420166bc
kishanshettyb <kishanqr@gmail.com>
2025-01-09 06:57:35 +0530

    fix: add domains hook

--- src/hooks/useDomains.ts
+++ src/hooks/useDomains.ts
@@ -1,16 +1,16 @@
-import { useGetAllDomains } from '@/services/queries/domains'
-import { Domain } from '@/types/commonTypes'
+import { useGetAllDomains } from "@/services/queries/domains";
+import { Domain } from "@/types/commonTypes";
 
 export const useDomains = () => {
-  const allDomainsData = useGetAllDomains()
-  const data: Domain[] = allDomainsData?.data?.data || []
+  const allDomainsData = useGetAllDomains();
+  const data: Domain[] = allDomainsData?.data?.data || [];
 
   const sortedData = data.sort((a, b) => {
-    const dateA = new Date(a.createdAt)
-    const dateB = new Date(b.createdAt)
+    const dateA = new Date(a.createdAt);
+    const dateB = new Date(b.createdAt);
 
-    return dateB.getTime() - dateA.getTime()
-  })
+    return dateB.getTime() - dateA.getTime();
+  });
 
-  return { data: sortedData }
-}
+  return { data: sortedData };
+};
