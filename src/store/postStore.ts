04347e3a907e1b31359fd4eaad177dc590a5fb3f
kishanshettyb <kishanqr@gmail.com>
2025-01-10 20:42:05 +0530

    feat: add post store

--- src/store/postStore.ts
+++ src/store/postStore.ts
@@ -4,16 +4,32 @@ import { create } from 'zustand'
 export type PostStoreState = {
   postId: string | null
   postStatus: 'draft' | 'published'
+
+  selectedDomains: string[] | undefined
+  selectedCategories: string[] | undefined
+  selectedTags: string[] | undefined
+
   setPostId: (postId: string | null) => void
   setPostStatus: (postStatus: 'draft' | 'published') => void
+
+  setSelectedDomains: (selectedDomains: string[] | undefined) => void
+  setSelectedCategories: (selectedCategories: string[] | undefined) => void
+  setSelectedTags: (selectedTags: string[] | undefined) => void
 }
 /* eslint-enable */
 
 const usePostStore = create<PostStoreState>((set) => ({
   postId: null,
   postStatus: 'draft',
+  selectedDomains: undefined,
+  selectedCategories: undefined,
+  selectedTags: undefined,
+
   setPostId: (postId) => set({ postId }),
-  setPostStatus: (postStatus) => set({ postStatus })
+  setPostStatus: (postStatus) => set({ postStatus }),
+  setSelectedDomains: (selectedDomains) => set({ selectedDomains }),
+  setSelectedCategories: (selectedCategories) => set({ selectedCategories }),
+  setSelectedTags: (selectedTags) => set({ selectedTags })
 }))
 
 export default usePostStore
