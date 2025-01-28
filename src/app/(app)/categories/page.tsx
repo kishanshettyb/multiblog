cfac3e891ac71344345f98f11c8e9724e90d6906
kishanshettyb <kishanqr@gmail.com>
2025-01-28 07:19:27 +0530

    fix: update categories page with create and Update

--- src/app/(app)/categories/page.tsx
+++ src/app/(app)/categories/page.tsx
@@ -6,24 +6,41 @@ import { CustomDataTable } from '@/components/customDatatable'
 import CreateCategoriesForm from '@/components/forms/createCategoriesForm'
 import { getCategoryColumns } from '@/config/categoriesColumns'
 import { useCategories } from '@/hooks/useCategories'
+import { CustomModal } from '@/components/customModal'
+import useModalStore from '@/store/store'
 
 function CategoriesPage() {
   const { data } = useCategories()
   const columns = getCategoryColumns()
+  const { setIsEditModalOpen, isEditModalOpen, documentId } = useModalStore()
+  const { setIsModalOpen, isModalOpen } = useModalStore()
 
   return (
     <div>
-      <Header
-        title="Categories"
-        desc=""
-        icon={Layers2}
-        buttons
-        buttonTitle="Create Category"
-        modalButton
-        modalTitle="Crete Categories"
-        components={<CreateCategoriesForm />}
-      />
+      <Header title="Categories" desc="" icon={Layers2} buttons modalButton />
       <CustomDataTable columns={columns} data={data} searchItem="category_name" />
+      <CustomModal
+        modalSize="md:max-w-[400px]"
+        title="Create Categories"
+        desc="Create categories for Domains"
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+      >
+        <div>
+          <CreateCategoriesForm />
+        </div>
+      </CustomModal>
+      <CustomModal
+        modalSize="md:max-w-[400px]"
+        title="Edit Categories"
+        desc="Edit categories for Domains"
+        isOpen={isEditModalOpen}
+        onClose={() => setIsEditModalOpen(false)}
+      >
+        <div>
+          <CreateCategoriesForm categoryId={documentId ? documentId : null} />
+        </div>
+      </CustomModal>
     </div>
   )
 }
