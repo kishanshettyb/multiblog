e1fea71658770bb110dd55edcf57ead4a520a9e8
kishanshettyb <kishanqr@gmail.com>
2025-01-28 07:17:35 +0530

    fix: fix store

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,13 +2,23 @@ import { create } from 'zustand'
 /* eslint-disable */
 export type ModalState = {
   isModalOpen: boolean
+  isEditModalOpen: boolean
+  documentId: string | null
+  openModal: (id: string | undefined) => void
   setIsModalOpen: (isModalOpen: boolean) => void
+  setIsEditModalOpen: (isModalOpen: boolean) => void
+  closeModal: () => void
 }
 /* eslint-enable */
 
 const useModalStore = create<ModalState>((set) => ({
   isModalOpen: false,
-  setIsModalOpen: (isModalOpen) => set({ isModalOpen })
+  isEditModalOpen: false,
+  documentId: null,
+  setIsModalOpen: (isModalOpen) => set({ isModalOpen }),
+  setIsEditModalOpen: (isEditModalOpen) => set({ isEditModalOpen }),
+  openModal: (id) => set({ isEditModalOpen: true, documentId: id }),
+  closeModal: () => set({ isEditModalOpen: false, documentId: null })
 }))
 
 export default useModalStore
