dd6c78f56b7c2c0bb802eee3898a8b85ca8fc747
kishanshettyb <kishanqr@gmail.com>
2025-01-28 07:37:24 +0530

    fix: fix columns

--- src/hooks/useCategories.ts
+++ src/hooks/useCategories.ts
@@ -1,15 +1,21 @@
-import { useGetAllCategories } from "@/services/queries/categories";
-import { Categories } from "@/types/commonTypes";
+import { useGetAllCategories, useGetSingeCategory } from '@/services/queries/categories'
+import { Categories } from '@/types/commonTypes'
 
 export const useCategories = () => {
-  const allCategoriesData = useGetAllCategories();
-  const data: Categories[] = allCategoriesData?.data?.data || [];
+  const allCategoriesData = useGetAllCategories()
+  const data: Categories[] = allCategoriesData?.data?.data || []
 
   const sortedData = data.sort((a, b) => {
-    const dateA = new Date(a.createdAt);
-    const dateB = new Date(b.createdAt);
+    const dateA = new Date(a.createdAt)
+    const dateB = new Date(b.createdAt)
 
-    return dateB.getTime() - dateA.getTime();
-  });
-  return { data: sortedData };
-};
+    return dateB.getTime() - dateA.getTime()
+  })
+  return { data: sortedData }
+}
+
+export const useSingleCategory = (categoryId: string) => {
+  const allCategoriesData = useGetSingeCategory(categoryId)
+  const data: Categories[] = allCategoriesData?.data?.data || []
+  return { data }
+}
