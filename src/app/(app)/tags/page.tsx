e0fc921e68ab93de5c89dfc8f50ac5a3681e89a0
kishanshettyb <kishanqr@gmail.com>
2025-01-29 16:59:00 +0530

    feat: add edit functionality to  domain and tag page

--- src/app/(app)/tags/page.tsx
+++ src/app/(app)/tags/page.tsx
@@ -6,10 +6,15 @@ import { CustomDataTable } from '@/components/customDatatable'
 import { useTags } from '@/hooks/useTags'
 import { getTagsColumns } from '@/config/tagsColumns'
 import CreateTagsForm from '@/components/forms/createTagsForm'
+import { CustomModal } from '@/components/customModal'
+import useModalStore from '@/store/store'
 
 function Tags() {
   const { data } = useTags()
   const columns = getTagsColumns()
+  const { setIsEditModalOpen, isEditModalOpen, documentId } = useModalStore()
+  const { isModalOpen, setIsModalOpen } = useModalStore()
+
   return (
     <div className="h-full overflow-auto ">
       <Header
@@ -18,11 +23,32 @@ function Tags() {
         buttons
         buttonTitle="Create tags"
         icon={Plus}
-        modalSize="md:max-w-[400px]"
         modalButton
-        modalTitle="Create tags"
-        components={<CreateTagsForm />}
       />
+
+      <CustomModal
+        modalSize="md:max-w-[400px]"
+        title="Create Tags"
+        desc="tags"
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+      >
+        <div>
+          <CreateTagsForm tagId={documentId ? documentId : null} />
+        </div>
+      </CustomModal>
+
+      <CustomModal
+        modalSize="md:max-w-[400px]"
+        title="Edit Tags"
+        desc="Edit Tags"
+        isOpen={isEditModalOpen}
+        onClose={() => setIsEditModalOpen(false)}
+      >
+        <div>
+          <CreateTagsForm tagId={documentId ? documentId : null} />
+        </div>
+      </CustomModal>
       <CustomDataTable columns={columns} data={data} searchItem="tag_name" />
     </div>
   )
